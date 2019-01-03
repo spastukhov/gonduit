a340ff3888eef268ef7a3108f0ce1d25c9e91eab
Simonas Galinis <simonas@uber.com>
2019-01-03 16:45:27 +0200

    add "email" attribute to "entity.user"

--- entities/user.go
+++ entities/user.go
@@ -5,6 +5,7 @@ type User struct {
 	PHID     string   `json:"phid"`
 	UserName string   `json:"userName"`
 	RealName string   `json:"realName"`
+	Email    string   `json:"email"`
 	Image    string   `json:"image"`
 	URI      string   `json:"uri"`
 	Roles    []string `json:"roles"`
